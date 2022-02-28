75d0e93a7c6f424b88cfef474af168d8c894b3e0
aabols11 <aabols11@mts.ru>
2022-02-28 23:46:07 +0300

    add tests to fastapi project

--- tests.py
+++ tests.py
@@ -9,3 +9,10 @@ def test_sum_endpoint():
     response = client.get("/add/?a=2&b=3")
     assert response.status_code == 200
     assert response.json() == {"sum": 5}
+
+
+def test_add_user():
+    response = client.post("/users/", json={"username": "IVAN"})
+    assert response.status_code == 200
+    assert response.json()['username'] == "IVAN"
+
